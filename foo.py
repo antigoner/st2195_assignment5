5e6b2d64e1355458401717affbc1ea69a9f3b028
Yap Cheu Yee <yapcheuyee@gmail.com>
2026-05-16 02:18:19 +0800

    some adjustments

--- foo.py
+++ foo.py
@@ -1,14 +1,17 @@
-# Checks whether x is divisible by k.
 def is_divisible_by_k(x, k):
+  '''
+  Checks whether x is divisible by k.
+  '''
   return x%k == 0
 
-# Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
-
 integers = [] # tuples () cannot be appended, only lists [] can
+
+# Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
 for i in range(1001): # python numbers are not inclusive
-  if is_divisible_by_k(i, 2) or is_divisible_by_k(i, 3) or is_divisible_by_k(i, 7):
-    integers.append(i)
+   if is_divisible_by_k(i, 2) or is_divisible_by_k(i, 3) or is_divisible_by_k(i, 7):
+      integers.append(i)
 
 # Sum all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
 total = sum(integers)
 print(total)
+
